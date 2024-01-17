04ecafaf3448fc46f3c38a3028dbb4b62598b029
Jiseok Hong <jiseok_hong_hkg@ensignfreight.com>
2024-01-17 16:30:15 +0800

    add custom user detail in security folder

---
+++ src/main/java/hjs/mall/security/CustomUserDetail.java
@@ -0,0 +1,57 @@
+package hjs.mall.security;
+
+import hjs.mall.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetail(Member member) {
+        this.member = member;
+    }
+
+    public Member getMember() {
+        return this.member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(member.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
