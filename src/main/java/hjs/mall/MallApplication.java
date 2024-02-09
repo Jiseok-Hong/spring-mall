978cc4f0b7e201cacdc80688deac5aaa85bfd598
Jiseok Hong <jiseok_hong_hkg@ensignfreight.com>
2024-02-09 12:25:55 +0800

    delete unused class

--- src/main/java/hjs/mall/MallApplication.java
+++ src/main/java/hjs/mall/MallApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MallApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MallApplication.class, args);
 	}
 
