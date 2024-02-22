470bf24dddd7b7167b39ea1176c30e7a175c8d70
Jiseok Hong <jiseok_hong_hkg@ensignfreight.com>
2024-02-22 14:55:35 +0800

    member jpa repository findby user name

--- src/main/java/hjs/mall/repository/MemberRepository.java
+++ src/main/java/hjs/mall/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package hjs.mall.repository;
 import hjs.mall.domain.Member;
 import org.springframework.data.jpa.repository.*;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<Member> findByUserName(String userName);
 }
